ebbbc037fe610d1d897e35d7a70cc7bb80e1ae29
tiberiu1999 <tiberiuiuliann@yahoo.com>
2021-03-03 20:11:02 +0200

    #1

--- GeometryDash/Assets/Scripts/PlayerScript.cs
+++ GeometryDash/Assets/Scripts/PlayerScript.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public float jumpPower = 10.0f;
     public bool isGrounded = false;
+    public float playerSpeed = 5f;
+    public GameObject gameOverText;
+
     Rigidbody2D myRigidbody;
     // Start is called before the first frame update
     void Start()
     {
+        gameOverText.SetActive(false);
         myRigidbody = transform.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //transform.Translate(Vector2.right * (Time.deltaTime * playerSpeed));
     } 
 
     void FixedUpdate()
@@ -26,12 +32,20 @@ public class PlayerScript : MonoBehaviour
         {
             myRigidbody.AddForce(Vector3.up * (jumpPower * myRigidbody.mass * myRigidbody.gravityScale * 20.0f));
         }
+        
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.collider.tag == "Ground")
             isGrounded = true;
+
+        if(other.gameObject.tag.Equals("Enemy"))
+        {
+            gameOverText.SetActive(true);
+            gameObject.SetActive(false);
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 
     void OnCollisionStay2D(Collision2D other)
