66b25d61c53c0a4d5713dfb83c46b8cde2fb43c7
tiberiu1999 <tiberiuiuliann@yahoo.com>
2021-03-10 22:04:14 +0200

    Added-TileSets-basic-level

--- GeometryDash/Assets/Scripts/PlayerScript.cs
+++ GeometryDash/Assets/Scripts/PlayerScript.cs
@@ -6,46 +6,70 @@ using UnityEngine.SceneManagement;
 public class PlayerScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float jumpPower = 10.0f;
     public bool isGrounded = false;
+    public float jumpPower = 10.0f;
     public float playerSpeed = 5f;
+    public float rotation_speed = 5f;
     public GameObject gameOverText;
-    
-    Rigidbody2D myRigidbody;
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         gameOverText.SetActive(false);
-        myRigidbody = transform.GetComponent<Rigidbody2D>();
+        rb = transform.GetComponent<Rigidbody2D>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.right * (Time.deltaTime * playerSpeed));
-    } 
+        //transform.Translate(Vector2.right * (Time.deltaTime * playerSpeed));
+    }
 
     void FixedUpdate()
     {
-        
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        //if(rb.velocity.magnitude < 1 && rb.velocity.magnitude > 0)
+        //    Debug.Log(rb.velocity.magnitude );
+
+        //if (Input.GetKey(KeyCode.Space) && isGrounded && rb.velocity.magnitude == 0)
+        //{
+        //    rb.AddForce(Vector3.up * (jumpPower * rb.mass * rb.gravityScale * 20.0f));
+        //}
+        Vector3 vel = rb.velocity;
+        vel.x = playerSpeed;
+        rb.velocity = vel;
+
+        if (isGrounded)
         {
-            myRigidbody.AddForce(Vector3.up * (jumpPower * myRigidbody.mass * myRigidbody.gravityScale * 20.0f));
+            rb.constraints = RigidbodyConstraints2D.None;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.velocity = Vector3.zero;
+                rb.AddForce(Vector2.up * jumpPower * rb.mass * rb.gravityScale * 20.0f);
+            }
         }
-        
+        else
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            transform.Rotate(Vector3.back * rotation_speed);
+        }
+
+        //if (rb.velocity.x != 7 && rb.velocity.x != 0)
+        //    GameOver();
+
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.collider.tag == "Ground")
-            isGrounded = true;
-
-        if(other.gameObject.tag.Equals("Enemy"))
         {
-            gameOverText.SetActive(true);
-            gameObject.SetActive(false);
-            SceneManager.LoadScene("SampleScene");
+            isGrounded = true;
+        }
+
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            GameOver();
         }
     }
 
@@ -58,6 +82,16 @@ public class PlayerScript : MonoBehaviour
     void OnCollisionExit2D(Collision2D other)
     {
         if (other.collider.tag == "Ground")
+        {
             isGrounded = false;
+        }
+
+    }
+
+    void GameOver()
+    {
+        gameOverText.SetActive(true);
+        gameObject.SetActive(false);
+        SceneManager.LoadScene("SampleScene");
     }
 }
