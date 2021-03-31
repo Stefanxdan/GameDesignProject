e449a0191abf69a5ca459e2b449751fef3ccebbe
tiberiu1999 <tiberiuiuliann@yahoo.com>
2021-03-31 19:05:11 +0300

    Added-MainMenu-Scene

---
+++ GeometryDash/Assets/Scripts/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Manager : MonoBehaviour
+{
+   
+    public void LoadScene0()
+    {
+        SceneManager.LoadScene(0);
+    }
+      
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
